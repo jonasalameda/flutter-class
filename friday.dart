19c88080f4569b6586d25fd82266c9fd59fe7c68
jonas <125613982+jonasalameda@users.noreply.github.com>
2025-09-08 13:23:51 -0400

    feat: add week2 friday file

---
+++ friday.dart
@@ -0,0 +1,11 @@
+void main() {
+  var list = ['Biel', 'Zanom', 'Tom', 'James'];
+  print("Examples of list");
+  list.forEach((item){
+    print('${list.indexOf(item)}: $item');
+  });
+}
+
+int fibonnaci(int num, int num2) {
+
+}
